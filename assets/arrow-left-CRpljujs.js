783940420ead87c5833ee9e2b65769ed8b13669e
markjin9981 <markjin9981@users.noreply.github.com>
2026-01-21 16:16:13 +0000

    deploy: 3f6b9ee25501cb059acff4395767dd9f2d99cccb

--- assets/arrow-left-CRpljujs.js
+++ assets/arrow-left-CRpljujs.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DbJyqxL-.js";/**
+import{c as e}from"./index-8rJOPdqN.js";/**
  * @license lucide-react v0.363.0 - ISC
  *
  * This source code is licensed under the ISC license.
